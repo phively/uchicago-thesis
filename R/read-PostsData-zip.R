abc4e36e931ab4180e6e3790d2941785288307b5
phively <pbhively@gmail.com>
2017-03-04 16:05:05 -0600

    Strip remaining non-text characters from body

--- R/read-PostsData-zip.R
+++ R/read-PostsData-zip.R
@@ -4,8 +4,19 @@ require(dplyr)
 # Load data
 data <- unz("data/PostsData.zip", filename = "posts.csv") %>% # Link to zipped file
   read.csv(header = TRUE, sep = ",", stringsAsFactors = FALSE) %>% # Read zipped file
-  select(Id, Name, AcceptedAnswerId, CleanBody) # Select fields needed for joins
+  select(Id, Name, ParentId, CleanBody) # Select fields needed for joins
 
-# Question and Answer dataframes
-#q <-
-#a <-
+# Question and Answer merged dataframe
+merged <- bind_rows(
+  # Questions
+  data %>% filter(Name == "Question") %>% select(Id, CleanBody),
+  # Answers, with associated question's ID inserted
+  data %>% filter(Name == "Answer") %>% select(Id = ParentId, CleanBody)
+) %>%
+  group_by(Id) %>% # Answers roll up into associated question
+  summarise_each(
+    funs(paste(., collapse = " ")) # Merge associated questions/answers
+  )
+
+# Cleanup
+remove(data)

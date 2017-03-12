8ad567454e6eb6db0078eb09633b7442aa009f7e
phively <pbhively@gmail.com>
2017-03-11 21:50:38 -0600

    R code whitespace formatting

--- R/read-PostsData-zip.R
+++ R/read-PostsData-zip.R
@@ -7,12 +7,13 @@ data <- unz("data/PostsData.zip", filename = "posts.csv") %>% # Link to zipped f
   select(Id, Name, ParentId, CleanBody) # Select fields needed for joins
 
 # Question and Answer merged dataframe
-merged <- bind_rows(
-  # Questions
-  data %>% filter(Name == "Question") %>% select(Id, CleanBody),
-  # Answers, with associated question's ID inserted
-  data %>% filter(Name == "Answer") %>% select(Id = ParentId, CleanBody)
-) %>%
+merged <-
+  bind_rows(
+    # Questions
+    data %>% filter(Name == "Question") %>% select(Id, CleanBody),
+    # Answers, with associated question's ID inserted
+    data %>% filter(Name == "Answer") %>% select(Id = ParentId, CleanBody)
+  ) %>%
   group_by(Id) %>% # Answers roll up into associated question
   summarise_each(
     funs(paste(., collapse = " ")) # Merge associated questions/answers

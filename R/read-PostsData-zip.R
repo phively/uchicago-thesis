8454324601357060d8bff2bd5e1de2196610c86a
phively <pbhively@gmail.com>
2017-03-19 19:10:47 -0500

    Sparse matrix with TeX preserved

--- R/read-PostsData-zip.R
+++ R/read-PostsData-zip.R
@@ -1,10 +1,27 @@
+# 2017-03-19 update: leave LaTeX code in the document
+
 # Required package(s)
 require(dplyr)
 
 # Load data
 data <- unz("data/PostsData.zip", filename = "posts.csv") %>% # Link to zipped file
   read.csv(header = TRUE, sep = ",", stringsAsFactors = FALSE) %>% # Read zipped file
-  select(Id, Name, ParentId, CleanBody) # Select fields needed for joins
+  select(Id, Name, ParentId, Title, Body) # Select fields needed for joins
+
+#
+data <- data %>% mutate(
+  CleanBody = paste(Title, Body, sep = " "), # Concatenate Title to left of Body
+  CleanBody = CleanBody %>%
+    gsub(pattern = "<code>.*?</code>", replacement = " ", x = .), # Strip <code> tags
+  CleanBody = CleanBody %>%
+    lapply(FUN = read_html) %>% # Read CleanBody as an html file
+    lapply(FUN = xml_text) %>% # Extract any text between valid <tags></tags>
+    unlist() %>%
+    # Remove URLs starting with http and ending with a space
+    str_replace_all("http[^[:space:]]*", " ") %>%
+    # Remove newlines
+    str_replace_all("[\r\n]" , " ")
+)
 
 # Question and Answer merged dataframe
 merged <-
